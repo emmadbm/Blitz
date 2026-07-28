e766c858c9a2ddf1d7fe2d9fea8c9a115df40dbf
emmadbm <emmadbm0612@gmail.com>
2026-07-28 09:54:41 +0530

    Starting the UI of Blitz

--- blitz.py
+++ blitz.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from api import main
 
 app = Flask(__name__)
 
 app.register_blueprint(main)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
-
